14fa3542da692fab111fc0a78c2b1a8258683d4d
DDurant94 <dannyjdurant@gmail.com>
2024-02-29 22:25:35 -0600

    assignment one

---
+++ Q-01-T-02.py
@@ -0,0 +1,13 @@
+'''
+Task 2: Your Mood Today
+Ask the user how they feel today. 
+If they say "happy", print "That's great to hear!", and if they say "sad", print "I hope your day gets better!". 
+Ensure your if statement is properly indented.
+'''
+
+user_mood = input("How are you feeling today? ")
+
+if user_mood == "happy":
+    print("That's great to hear!")
+else:
+    print("I hope your day gets better!")
